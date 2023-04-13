f5b313735e22428c331484f4a0ff84e89db8ea94
NooshineTalebi <talebinooshine@gmail.com>
2023-04-13 18:42:46 +0430

    fifth commit

--- script.js
+++ script.js
@@ -17,3 +17,28 @@ toggle.onclick = function(){
     toggle.classList.toggle('active');
     sidebar.classList.toggle('active');
 }
+
+
+
+const menuTeam = document.querySelector(".team-item");
+const teamSection = document.querySelector(".team");
+menuTeam.addEventListener('click' , () => {
+    teamSection.scrollIntoView();
+})
+
+
+
+const LunchSection = document.querySelector(".lunch-section");
+LunchSection.addEventListener("click" , ()=>{
+    const menuItems = document.querySelectorAll(".menu-item");
+    menuItems.forEach((menu) => {
+        if(menu.classList.includes(".menu-lunch")){
+            menu.classList.add("active");
+        }else{
+            menu.classList.add("hidden")
+        }
+    })
+})
+
+
+
